88ec5ba708145759ae6b6996f37599a47e329b20
tacorda015 <eduardotacorda17@gmail.com>
2024-02-11 14:46:10 +0800

    Add connection sidebar to home.php

--- Navigation/SideBar.php
+++ Navigation/SideBar.php
@@ -6,7 +6,7 @@
     <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="home.php">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="course_subject.php">Course/Subject</a>
